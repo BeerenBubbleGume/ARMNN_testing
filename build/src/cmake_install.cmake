fc1c8ff3c953e71bcb73c36679a496435365493e
BeerenBubbleGume <dr.hazdan1999@gmail.com>
2022-10-18 10:48:33 +0000

    commit

--- build/src/cmake_install.cmake
+++ build/src/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/beerenbubblegum/Desktop/armnn-testing/src
+# Install script for directory: /home/orangepi/ARMNN_testing/src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+    set(CMAKE_INSTALL_CONFIG_NAME "")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -44,8 +44,8 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/beerenbubblegum/Desktop/armnn-testing/build/src/source/cmake_install.cmake")
-  include("/home/beerenbubblegum/Desktop/armnn-testing/build/src/include/cmake_install.cmake")
+  include("/home/orangepi/ARMNN_testing/build/src/source/cmake_install.cmake")
+  include("/home/orangepi/ARMNN_testing/build/src/include/cmake_install.cmake")
 
 endif()
 
--- build/src/include/cmake_install.cmake
+++ build/src/include/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/beerenbubblegum/Desktop/armnn-testing/src/include
+# Install script for directory: /home/orangepi/ARMNN_testing/src/include
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+    set(CMAKE_INSTALL_CONFIG_NAME "")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
