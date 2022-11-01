d1be799e25f4642367160855ff0308e3a3b244a3
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2022-11-01 17:28:45 +0500

    commit

--- src/source/utils.cpp
+++ src/source/utils.cpp
@@ -1,8 +1,8 @@
 #include "../include/utils.hpp"
 
-#include "VerificationHelpers.hpp"
-#include "ImageTensorGenerator.hpp"
-#include "InferanceImage.hpp"
+#include "../include/VerificationHelpers.hpp"
+#include "../include/ImageTensorGenerator.hpp"
+#include "../include/InferanceImage.hpp"
 
 vector<string> ABC::get_classes(string classes_path){
     ifstream in(classes_path);
