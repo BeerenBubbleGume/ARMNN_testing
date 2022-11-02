e61d76011349c94d9d743dd1edcc5e4f9ddd07ff
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2022-11-02 09:54:16 +0500

    commit

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char* argv[]){
     if(argc > 0){
-        TRTModule* nn = new TRTModule("weights/yolox-tiny640.onnx", "classes.txt");
+        TRTModule* nn = new TRTModule("weights/yolox-tiny.onnx", "classes.txt");
         nn->startNN(argv[0], argv[1], 35);
         display_process_time();
         return 0;
