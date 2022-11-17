bbdff73ea661f974a4c02ac41d2e9ff76eff6059
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2022-11-17 14:01:45 +0500

    commit

--- src/source/utils.cpp
+++ src/source/utils.cpp
@@ -102,7 +102,7 @@ vector<nc::NdArray<float>> TRTModule::trtInference(nc::NdArray<float> inputData,
                         ortInputs, 
                         session->GetOutputNames())[0]);
                         
-    return box->preprocess(nc::NdArray<float>(ortInputs.data()->GetTensorRawData()), imageShape, imgz);
+    return box->preprocess(nc::NdArray<Ort::Value>(ortInputs), imageShape, imgz);
 }
 void TRTModule::startNN(string videoSrc, string outputPath, int fps){
     auto cap = cv::VideoCapture(videoSrc);
