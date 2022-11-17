b479b1bed4aecc2ef68f61ff0f86640ae5ff9d0f
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2022-11-17 12:34:05 +0500

    commit

--- src/source/utils.cpp
+++ src/source/utils.cpp
@@ -93,8 +93,10 @@ nc::NdArray<float> ABC::preprocessInput(nc::NdArray<float> image){
 
 vector<nc::NdArray<float>> TRTModule::trtInference(nc::NdArray<float> inputData, nc::NdArray<float> imgz){
     vector<Ort::Value> ortInputs;
-    ortInputs.push_back(Ort::Experimental::Value::CreateTensor<float>((inputData[inputData.none(), inputData.rSlice(), inputData.rSlice(), inputData.rSlice()]).toStlVector().data(),
-                        inputData.toStlVector().size(), imgz.toStlVector()));
+    ortInputs.push_back(Ort::Experimental::Value::CreateTensor<float>(
+                        (inputData[inputData.none(), inputData.rSlice(), inputData.rSlice(), inputData.rSlice()]).toStlVector().data(),
+                        inputData.toStlVector().size(), 
+                        reinterpret_cast<vector<int64_t>(imgz.toStlVector())));
     ortInputs = session->Run(session->GetInputNames(), 
                         ortInputs, 
                         session->GetOutputNames());
