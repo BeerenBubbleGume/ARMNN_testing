773f1142a8fe8f3e7f40dcb763ed6c76372b1ebc
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2022-11-20 14:45:51 +0500

    commit

--- src/source/utils.cpp
+++ src/source/utils.cpp
@@ -30,9 +30,8 @@ nc::NdArray<float> ABC::letterbox(cv::Mat image, vector<float> expected_size){
     
     cv::resize(image, image, cv::Size(nw, nh), 0.0, 0.0, cv::INTER_CUBIC);
     nc::NdArray<float> newImage = nc::full(nc::Shape(eh, ew), 128.f);
-    newImage[nc::floor_divide((eh - nh), (nc::floor_divide(newImage(2, nc::int32(eh - nh)), newImage[2 + nh]))), 
-            nc::floor_divide((ew - nw), (nc::floor_divide(newImage(2, nc::int32(ew - nw)), newImage[2 + nw]))), 
-            newImage.rSlice()] = nc::copy(nc::NdArray<float>(image.begin<float>(), image.end<float>()));
+    newImage(nc::floor_divide(int(eh - nh), int(nc::floor_divide(newImage(2, nc::int32(eh - nh)), newImage[2 + nh]))), 
+            nc::floor_divide(int(ew - nw), int(nc::floor_divide(newImage(2, nc::int32(ew - nw)), newImage[2 + nw])))) = *nc::copy(nc::NdArray<float>(image.begin<float>(), image.end<float>())).data();
     
     return newImage;
 }
