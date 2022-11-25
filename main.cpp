63d0723b91d595977d25530d33d7b0c67aa6640f
Daniil Khaziev <54493389+BeerenBubbleGume@users.noreply.github.com>
2022-11-25 16:06:07 +0500

    commit

--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 
 int main(int argc, char* argv[]){
     if(argc > 1){
-        TRTModule* nn = new TRTModule("weights/yolox-tiny640.onnx", "classes.txt");
+        TRTModule* nn = new TRTModule("weights/yolox-tiny.onnx", "classes.txt");
         nn->startNN(argv[1], argv[2], 35);
         display_process_time();
         return 0;
     }
     else {
-        TRTModule* nn = new TRTModule("weights/yolox-tiny640.onnx", "classes.txt");
+        TRTModule* nn = new TRTModule("weights/yolox-tiny.onnx", "classes.txt");
         nn->startNN("video/v.avi", "v-out.avi", 35);
         display_process_time();
     }
